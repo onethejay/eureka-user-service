704bdc0fca348d6b51ab21a6e7a56c51db31720c
onethejay <jhjang951@gmail.com>
2022-04-11 22:51:19 +0900

    JWT, Security Config 추가

---
+++ src/main/java/com/tistory/onethejay/userservice/config/WebSecurity.java
@@ -0,0 +1,42 @@
+package com.tistory.onethejay.userservice.config;
+
+import com.tistory.onethejay.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final Environment env;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("/actuator/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")
+                .hasIpAddress("127.0.0.1")   // <- IP 변경
+                .and()
+                .addFilter(getAuthenticationFilter());
+
+        http.headers().frameOptions().disable();    //H2 console
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        return new AuthenticationFilter(authenticationManager(), userService, env);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+    }
+}
+
