713e9ef0d0db5e7e8fd543ab63a6ce6a0da69401
onethejay <jhjang951@gmail.com>
2022-04-21 23:29:02 +0900

    actuator 적용

--- src/main/java/com/tistory/onethejay/userservice/controller/UserController.java
+++ src/main/java/com/tistory/onethejay/userservice/controller/UserController.java
@@ -8,6 +8,7 @@ import com.tistory.onethejay.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +20,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/")
 public class UserController {
+    private final Environment env;
     private final UserService userService;
+
+    @GetMapping("/health_check")
+    public String healthCheck() {
+        return "It's Working in User Service"
+                + ", port(local.server.port)=" + env.getProperty("local.server.port")
+                + ", port(server.port)=" + env.getProperty("server.port")
+                + ", token secret=" + env.getProperty("token.secret")
+                + ", token expiration time=" + env.getProperty("token.expiration_time");
+    }
 
     @GetMapping("welcome")
     public String welcome() {
